d6a8cf29d2c9f33b5043474cf8ebf73f08adddcf
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-09 05:30:50 +0200

    fogorow: import odm sku

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,13 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(sort $(BOARD_VENDOR_RAMDISK_KERNEL_MODU
 BOARD_VENDOR_KERNEL_MODULES_LOAD += $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor))
 BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(KERNEL_PATH)/fogorow/vendor-modules/*.ko)
 
+# SKU
+ODM_MANIFEST_SKUS += nfc_dsds nfc_ss nonfc_dsds nonfc_ss
+ODM_MANIFEST_NFC_DSDS_FILES := $(DEVICE_PATH)/manifest_nfc_dsds.xml
+ODM_MANIFEST_NFC_SS_FILES := $(DEVICE_PATH)/manifest_nfc_ss.xml
+ODM_MANIFEST_NONFC_DSDS_FILES := $(DEVICE_PATH)/manifest_nonfc_dsds.xml
+ODM_MANIFEST_NONFC_SS_FILES := $(DEVICE_PATH)/manifest_nonfc_ss.xml
+
 # inherit from common mt6768-common
 -include device/motorola/mt6768-common/BoardConfigCommon.mk
 
