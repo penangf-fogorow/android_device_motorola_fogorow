5e30ccf33b6b48fcb4e929147e2f891fe6403388
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-09 14:27:35 +0200

    fogorow: build required NFC interfaces

--- device.mk
+++ device.mk
@@ -7,5 +7,10 @@
 # Inherit from common device makefile.
 $(call inherit-product, device/motorola/mt6768-common/device.mk)
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service \
+    libchrome.vendor
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/fogorow/fogorow-vendor.mk)
