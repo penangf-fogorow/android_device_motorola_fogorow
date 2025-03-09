b49b30191f6260c4112ac772cd70c151d96883f2
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-09 03:12:39 +0200

    penangf: define assert

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/motorola/fogorow
 
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := fogorow
+
 # inherit from common mt6768-common
 -include device/motorola/mt6768-common/BoardConfigCommon.mk
 
